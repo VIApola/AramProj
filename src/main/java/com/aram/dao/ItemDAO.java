cea239536ee3ff8cbb26fafe4055af472bcb3e38
VIApola <kgh9201@gmail.com>
2022-05-27 17:54:22 +0900

    category.jsp created_2

--- src/main/java/com/aram/dao/ItemDAO.java
+++ src/main/java/com/aram/dao/ItemDAO.java
@@ -26,42 +26,4 @@ public class ItemDAO {
 	}
 
 	
-	public ArrayList<ItemDTO> selectAll() throws Exception{
-		String sql ="select * from tbl_items";
-		try(Connection con = bds.getConnection();
-			PreparedStatement pstmt = con.prepareStatement(sql)	
-					){
-			
-			ResultSet rs = pstmt.executeQuery();
-			
-			ArrayList<ItemDTO> list = new ArrayList<>();
-			
-			while(rs.next()) {
-				int item_id = rs.getInt("item_id");
-				String item_name = rs.getString("item_name");
-				int price = rs.getInt("price");
-				String item_comment = rs.getString("item_comment");
-				String item_regdate = rs.getString("item_regdate");
-				int item_stock = rs.getInt("item_stock");
-				String category_id = rs.getString("category_id");
-				int img_no = rs.getInt("img_no");
-				
-				list.add(new ItemDTO(item_id, item_name, price, item_comment, item_regdate, item_stock,category_id,img_no));
-			}
-			return list;
-		}
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
