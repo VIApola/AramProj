b5ae873341890dff3cb79916ddf2767965f1d26c
VIApola <kgh9201@gmail.com>
2022-05-27 20:06:44 +0900

    ImgFileDAO created

--- src/main/java/com/aram/controller/HomeController.java
+++ src/main/java/com/aram/controller/HomeController.java
@@ -1,11 +1,14 @@
 package com.aram.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.aram.dao.ImgFileDAO;
 
 @WebServlet("/")
 public class HomeController extends HttpServlet {
@@ -20,6 +23,8 @@ public class HomeController extends HttpServlet {
 	
 	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.sendRedirect("/main.jsp");
+		
+		
 	}
 
 }
