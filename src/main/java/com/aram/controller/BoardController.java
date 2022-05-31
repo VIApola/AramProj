574ed976c6c33c5f3754c659df1f7eb9b554718e
YeJiDev <hanyj1994@gmail.com>
2022-05-31 10:38:38 +0900

    Create NoticeDAO, Controller, / Modify JSPfile

--- src/main/java/com/aram/controller/BoardController.java
+++ src/main/java/com/aram/controller/BoardController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.aram.dao.NoticeDAO;
 import com.aram.dto.NoticeDTO;
+import com.google.gson.Gson;
 
 @WebServlet("*.bo")
 public class BoardController extends HttpServlet {
@@ -30,18 +31,65 @@ public class BoardController extends HttpServlet {
 		request.setCharacterEncoding("utf-8");
 		
 		if(uri.equals("/notice.bo")) { // 게시글 뿌려주기
-			System.out.println("얍!");
 			NoticeDAO dao = new NoticeDAO();
 			
 			try {
 				ArrayList<NoticeDTO> list = dao.selectAll();
+				System.out.println(list);
 				request.setAttribute("list", list);
 			}catch(Exception e) {
 				e.printStackTrace();
 			}
 			request.getRequestDispatcher("/board/notice.jsp").forward(request, response);
+			
+
 		}else if(uri.equals("/write.bo")) { // 게시굴 작성
 			response.sendRedirect("/board/write.jsp");
+		}else if(uri.equals("/searchProc.bo")) { // 글쓴이 검색 요청
+
+			
+			String searchAuthor = request.getParameter("searchauthor");
+			String searchTitle = request.getParameter("searchtitle");
+			String searchContent = request.getParameter("searchcontent");
+			System.out.println("searchAuthor : " + searchAuthor);
+			System.out.println("searchTitle : " + searchTitle);
+			System.out.println("searchContent : " + searchContent);
+				
+			NoticeDAO dao = new NoticeDAO();
+			try {
+				
+				if(!searchAuthor.equals(null)) {
+					ArrayList<NoticeDTO> list = dao.searchByAuthor(searchAuthor);
+					Gson gson = new Gson();
+					String rs = gson.toJson(list);
+					System.out.println(rs);
+					response.setCharacterEncoding("utf-8");
+					response.getWriter().append(rs);
+					
+				
+				}else if(!searchTitle.equals(null)){
+					ArrayList<NoticeDTO> list = dao.searchByTitle(searchTitle);
+					Gson gson = new Gson();
+					String rs = gson.toJson(list);
+					System.out.println(rs);
+					response.setCharacterEncoding("utf-8");
+					response.getWriter().append(rs);
+					
+					
+				}else if(!searchContent.equals(null)) {
+					ArrayList<NoticeDTO> list = dao.searchByContent(searchContent);
+					Gson gson = new Gson();
+					String rs = gson.toJson(list);
+					System.out.println(rs);
+					response.setCharacterEncoding("utf-8");
+					response.getWriter().append(rs);
+					
+				}
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			
 		}
 	}
 }
