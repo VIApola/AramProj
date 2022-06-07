eeb7a6d5f5d361311e709044c00e380c705e7382
YeJiDev <hanyj1994@gmail.com>
2022-06-07 15:46:11 +0900

    Modifying etc....

--- src/main/java/com/aram/controller/BoardController.java
+++ src/main/java/com/aram/controller/BoardController.java
@@ -64,13 +64,13 @@ public class BoardController extends HttpServlet {
 			
 			try {
 				
-				// HashMap map = dao.getPageNavi(curPage);
+//				 HashMap map = dao.getPageNavi(curPage);
 				
 				ArrayList<NoticeDTO> list = dao.selectAll(curPage*10-9, curPage*10);
 				System.out.println(list);
 				request.setAttribute("list", list);
 
-				request.setAttribute("naviMap", map);
+//				request.setAttribute("naviMap", map);
         
 				request.getSession();
 
@@ -286,15 +286,16 @@ public class BoardController extends HttpServlet {
 					response.setCharacterEncoding("utf-8");
 					response.getWriter().append(rs);
 					
-				}else if(session_id != null) {// 세션아이디로 자기자신이 쓴 글 조회
-					
-					ArrayList<QnaDTO> list = dao.searchByUserId(session_id);
-					Gson gson = new Gson();
-					String rs = gson.toJson(list);
-					System.out.println(rs);
-					response.setCharacterEncoding("utf-8");
-					response.getWriter().append(rs);
 				}
+//				else if(session_id != null) {// 세션아이디로 자기자신이 쓴 글 조회
+//					
+//					ArrayList<QnaDTO> list = dao.searchByUserId(session_id);
+//					Gson gson = new Gson();
+//					String rs = gson.toJson(list);
+//					System.out.println(rs);
+//					response.setCharacterEncoding("utf-8");
+//					response.getWriter().append(rs);
+//				}
 				
 			}catch(Exception e) {
 				e.printStackTrace();
