e4be4a4529e5cf4d2eb8e4c216e93ebb28fed8d1
bontaedev <devbontae9@gmail.com>
2022-06-07 15:16:47 +0900

    fix errors in UserController

--- src/main/java/com/aram/controller/UserController.java
+++ src/main/java/com/aram/controller/UserController.java
@@ -291,14 +291,6 @@ public class UserController extends HttpServlet {
 
 				if(session_id.equals(id)) {
 					UserDAO dao = new UserDAO();
-
-//					int rs = dao.deleteUser(id, pw);
-//					if(rs > 0) {
-//						
-//					}
-//				}else {
-//					
-
 				}
 			}catch(Exception e) {
 				e.printStackTrace();
